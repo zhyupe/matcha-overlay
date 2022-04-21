6c15df6dde7c5b2acd08836588f6ab005c5e27b4
Innpaul Zhang <zyp108421@gmail.com>
2022-04-22 01:24:54 +0800

    feat(welcome): add version check

--- src/app/notice/welcome/index.tsx
+++ src/app/notice/welcome/index.tsx
@@ -1,9 +1,30 @@
+import { useMemo } from 'react'
+import { Dialog } from '../../../components/dialog'
 import { getSeasonFooter, getSeasonTitle } from '../../../lib/season'
 import { OverlayProps } from '../../interface'
 import './index.css'
 
 const year = new Date().getFullYear()
 const build = process.env.build
+
+const appointedVersion = '22.4.15.1906'
+const parseVersion = (version: string) => version.split('.').map((item) => +item)
+function VersionCheck({ version }: { version: string | undefined }) {
+  const showNotice = useMemo(() => {
+    if (!version) return false
+
+    const current = parseVersion(version)
+    const appointed = parseVersion(appointedVersion)
+
+    for (let i = 0; i < 4; ++i) {
+      if (current[i] < appointed[i]) return true
+    }
+
+    return false
+  }, [version])
+
+  return showNotice ? <Dialog direction="top-left">您的插件版本较旧，请到插件中心更新</Dialog> : null
+}
 
 export function WelcomeNotice({ version, language, active }: OverlayProps) {
   if (!active) return null
@@ -23,6 +44,7 @@ export function WelcomeNotice({ version, language, active }: OverlayProps) {
       <p>
         本地版本：{version || '<等待数据>'} / 数据语言：{language}
       </p>
+      <VersionCheck version={version} />
       <p className="copyright">
         &copy; {year} FFCafe / Build: {build}
         {getSeasonFooter()}
