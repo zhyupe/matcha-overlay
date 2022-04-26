466da89eab6bf952e5c4b90de3336a42ad998f23
Innpaul Zhang <zyp108421@gmail.com>
2022-04-26 21:54:28 +0800

    chore: fix github pages build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 const buildTime = Date.now() - new Date(2020, 0, 1).getTime()
 export default defineConfig({
+  base: '',
   define: {
     'process.env.build': `'${pkg.version}-${Math.floor(buildTime / 1000 / 60 / 60)}'`,
     'process.env.POST_MOOGLE_API': `'${process.env.POST_MOOGLE_API}'`,
