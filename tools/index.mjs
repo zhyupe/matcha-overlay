a13defa4603523d63d67cb3990815dca55f6161e
zhyupe <zyp108421@gmail.com>
2026-06-13 22:28:49 +0800

    feat: update materia icon tool

--- tools/index.mjs
+++ tools/index.mjs
@@ -1,7 +1,9 @@
 import { updateMap } from './map.mjs'
+import { updateMateriaIcons } from './materia-icons.mjs'
 import { updateTreasure } from './treasure.mjs'
 import { updateWorld } from './world.mjs'
 
 await updateMap()
 await updateTreasure()
 await updateWorld()
+await updateMateriaIcons()
--- tools/materia-icons.mjs
+++ tools/materia-icons.mjs
@@ -1,37 +1,56 @@
-import { writeFileSync } from 'fs'
-import { join } from 'path'
-import { fileURLToPath } from 'url'
+import { writeFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const output = fileURLToPath(new URL('../public/icon/materia', import.meta.url))
-const css = 'https://lds-img.finalfantasyxiv.com/h/T/uO7XkVAGwkyx_gUrfgvedPRK_c.css'
+const page = 'https://jp.finalfantasyxiv.com/lodestone/character/'
 
-const res = await fetch(css)
-const style = await res.text()
-const files = []
+const cssRegex = /<link href="([^"]+\.css)" rel="stylesheet">/g
+const materiaRegex =
+  /\.db.+?__materia__(\w+) \.(\w+)(?::after)?[\s\S]+?url\((.+?)\)/gm
 
-const regex = /\.db.+?__materia__(\w+) \.(\w+)(?:\:after)?[\s\S]+?url\((.+?)\)/gm
-const count = {
-  normal: 0,
-  overmeld: 0,
+export async function updateMateriaIcons() {
+  const html = await fetch(page).then((res) => res.text())
+  const cssMatch = html.matchAll(cssRegex)
+  for (const item of cssMatch) {
+    const link = item[1]
+    console.log(link)
+
+    const res = await fetch(link)
+    const style = await res.text()
+    const materiaMatch = style.matchAll(materiaRegex)
+
+    const files = []
+    const count = {
+      normal: 0,
+      overmeld: 0,
+    }
+
+    for (const match of materiaMatch) {
+      const type = match[1] === 'normal' ? 'normal' : 'overmeld'
+      const index =
+        match[2] === 'socket'
+          ? 'empty'
+          : `grade${(count[type]++).toString().padStart(2, '0')}`
+
+      files.push({
+        url: new URL(match[3], link).href,
+        name: `slot_${type}_${index}.png`,
+      })
+    }
+
+    console.log(files)
+    if (files.length) {
+      Promise.all(
+        files.map(async ({ url, name }) => {
+          const res = await fetch(url)
+          const buf = await res.arrayBuffer()
+
+          writeFileSync(join(output, name), Buffer.from(buf))
+        }),
+      )
+
+      break
+    }
+  }
 }
-
-let match
-while ((match = regex.exec(style))) {
-  const type = match[1] === 'normal' ? 'normal' : 'overmeld'
-  const index = match[2] === 'socket' ? 'empty' : `grade${(count[type]++).toString().padStart(2, '0')}`
-
-  files.push({
-    url: new URL(match[3], css).href,
-    name: `slot_${type}_${index}.png`,
-  })
-}
-
-console.log(files)
-Promise.all(
-  files.map(async ({ url, name }) => {
-    const res = await fetch(url)
-    const buf = await res.arrayBuffer()
-
-    writeFileSync(join(output, name), Buffer.from(buf))
-  }),
-)
